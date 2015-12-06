6ddc225894bea837aaa77c8791f52f2ee5554043
klab <klab.ltd@gmail.com>
2015-12-07 04:53:49 +0900

    Add code generator.

---
+++ src/Klab.System.Code.JS/app.js
@@ -0,0 +1,55 @@
+﻿"use strict";
+
+class Code {
+    static getStatical(seed, enableSpecialASCIISet) {
+        return Code._get(seed, enableSpecialASCIISet);
+    }
+
+    static getRandomly(seed, enableSpecialASCIISet) {
+        var seedNum = Math.random().toString();
+        var seedTime = Date.now().toString();
+        return Code._get(seedNum + seedTime + seed, enableSpecialASCIISet);
+    }
+
+    static _get(seed, enableSpecialASCIISet) {
+        var enableASCIISet = Code._enableDefaultASCIISet;
+        if ((typeof enableSpecialASCIISet) === 'string') {
+            for (var i = 0; i < enableSpecialASCIISet.length; i++) {
+                var num = enableSpecialASCIISet[i].charCodeAt(0);
+                if (num >= 0 && num <= 126)
+                    enableASCIISet += enableSpecialASCIISet[i];
+            }
+        }
+
+        var sha = require('crypto');
+        var calc = sha.createHash('sha512');
+        calc.update(seed, 'utf8');
+        var raw = calc.digest();
+        for (var i = 0; i < raw.length; i++)
+            raw[i] = 255 - raw[i];
+
+        calc = sha.createHash('sha512');
+        calc.update(raw);
+        raw = calc.digest();
+        var code = '';
+        for (var i = 0; i < raw.length; i++) {
+            var c = String.fromCharCode(raw[i]);
+            if (enableASCIISet.indexOf(c) < 0)
+                code += raw[i].toString('16').toLowerCase().slice(-1);
+            else
+                code += c;
+        }
+
+        return code;
+    }
+
+    static get _enableDefaultASCIISet() {
+        return '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    }
+
+    static get printableSpecialASCIISet() {
+        return '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+    }
+}
+
+exports.code = Code;
---
+++ src/Klab.System.Code/Code.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klab.System.Code
+{
+    /// <summary>
+    /// ASCII code generator.
+    /// </summary>
+    public class Code
+    {
+        private Code() { }
+
+        public static string GetStatical(string seed, IEnumerable<char> enableSpecialASCIISet = null)
+        {
+            return Get(seed, enableSpecialASCIISet);
+        }
+
+        public static string GetRandomly(string seed, IEnumerable<char> enableSpecialASCIISet = null)
+        {
+            var seedNum = new Random().Next();
+            var seedTime = DateTime.Now;
+            return Get($"{seedNum}{seedTime.Ticks}{seed}", enableSpecialASCIISet);
+        }
+
+        private static string Get(string seed, IEnumerable<char> enableSpecialASCIISet = null)
+        {
+            var enableASCIISet = EnableDefaultASCIISet;
+            if (enableSpecialASCIISet != null)
+            {
+                foreach (var c in enableSpecialASCIISet.Where(x => x >= 0 && x <= 126))
+                    enableASCIISet.Add(c);
+            }
+
+            var calc = new SHA512Managed();
+            var raw = calc.ComputeHash(Encoding.UTF8.GetBytes(seed));
+            calc.Initialize();
+            raw = raw.Select(x => (byte)(255 - x)).ToArray();
+            raw = calc.ComputeHash(raw);
+            calc.Dispose();
+
+            var hexFormat = "x2";
+            var sb = new StringBuilder();
+            foreach (var c in raw.Select(x => (int)x))
+            {
+                sb.Append(enableASCIISet.Contains(c)
+                    ? (char)c
+                    : c.ToString(hexFormat)[1]
+                );
+            }
+
+            return sb.ToString();
+        }
+
+        private static HashSet<int> EnableDefaultASCIISet = new HashSet<int>(new[]
+        {
+            // 0 to 9
+            48, 49, 50, 51, 52, 53, 54, 55, 56, 57
+
+            // A to Z
+            , 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90
+
+            // a to z
+            , 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122
+        });
+
+        public static HashSet<char> PrintableSpecialASCIISet = new HashSet<char>(new[]
+        {
+            '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/'
+            , ':', ';', '<', '=', '>', '?', '@'
+            , '[', '\\', ']', '^', '_', '`'
+            , '{', '|', '}', '~'
+        });
+    }
+}
